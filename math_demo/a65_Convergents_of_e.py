6776c686abf05a3f2fc20d4ff8af41d883558353
zongff <zongff@tiansu-china.com>
2018-05-17 14:23:18 +0800

    65 problems

--- math_demo/a65_Convergents_of_e.py
+++ math_demo/a65_Convergents_of_e.py
@@ -1,41 +1,8 @@
-# coding:utf-8
-import math
-from fractions import Fraction
-def property_method(s,m,d):
-    a1 = int((math.sqrt(s)+m)/d)
-    m1 = a1*d-m
-    d1 = (s-m1**2)/d
-    print a1,m1,d1
-    a = str(a1+Fraction(1,s))
-    return a
+#Project Euler Problem 65
 
-token={1:1}
+n0, n1, L = 1, 2, 100
 
+for i in xrange(2, L+1):
+    n0, n1 = n1, n0 + n1 * (1 if i%3 else 2 * i//3)
 
-def fraction_method(num):
-    i = 1
-    s = num
-    if num in token:
-        return token[num]
-    if num%3==2:
-        token[s] = (num*2)/3+1
-        return token[s]
-    if i != num:
-        num -= 1
-        token[s] = 2 + Fraction(1, fraction_method(num))
-        return token[s]
-    else:
-        return 2
-
-
-def square_method(num):
-    # count = 0
-    i =1
-    while i <= num:
-        tmp = 2+Fraction(1,fraction_method(i))
-        # if len(str(Fraction(tmp).numerator)) > len(str(Fraction(tmp).denominator)):
-        #     count +=1
-        i += 1
-    return tmp
-print square_method(2)
-# print property_method(2,0,1)
+print "Project Euler 65 Solution =", sum(map(int, str(n1)))
